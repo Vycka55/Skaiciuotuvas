ad14c409a682428ddaa1f9a0e95c8fc565275b02
Vycka55 <82957006+Vycka55@users.noreply.github.com>
2022-10-14 13:40:28 +0300

    2022-10-14

--- main.py
+++ main.py
@@ -1,18 +1,53 @@
-import tkinter
 from tkinter import *
-from tkinter import ttk
-
-root = Tk(className='Skaičiuotuvas')
-# set window size
-root.geometry("500x500")
-root.minsize(500, 500)
-root.maxsize(500, 500)
-
-root.columnconfigure(0, weight=0)
-root.columnconfigure(1, weight=0)
-
-#text boxas aaa
+from tkinter import scrolledtext
 
 
+langas=Tk()
 
-root.mainloop()
+# Parent widget for the buttons
+
+
+tekst = Entry()
+tekst.grid(row=0, column=0)
+
+buttons_frame = Frame(langas)
+buttons_frame.grid(row=1, column=0, sticky=W+E) 
+
+
+btn_7 = Button(buttons_frame, text='7')
+btn_7.grid(row=1, column=0, padx=(0), pady=0)
+
+btn_8 = Button(buttons_frame, text='8')
+btn_8.grid(row=1, column=1, padx=(0), pady=0)
+
+btn_9 = Button(buttons_frame, text='9')
+btn_9.grid(row=1, column=2, padx=(0), pady=0)
+
+btn_x = Button(buttons_frame, text='X')
+btn_x.grid(row=1, column=3, padx=(0), pady=0)
+
+btn_4 = Button(buttons_frame, text='4')
+btn_4.grid(row=2, column=0, padx=(0), pady=0)
+
+btn_5 = Button(buttons_frame, text='5')
+btn_5.grid(row=2, column=1, padx=(0), pady=0)
+
+btn_6 = Button(buttons_frame, text='6')
+btn_6.grid(row=2, column=2, padx=(0), pady=0)
+
+btn_min = Button(buttons_frame, text='-')
+btn_min.grid(row=2, column=3, padx=(1), pady=1)
+
+btn_1 = Button(buttons_frame, text='1')
+btn_1.grid(row=3, column=0, padx=(1), pady=1)
+
+btn_2 = Button(buttons_frame, text='2')
+btn_2.grid(row=3, column=1, padx=(1), pady=1)
+
+btn_3 = Button(buttons_frame, text='3')
+btn_3.grid(row=3, column=2, padx=(1), pady=1)
+
+btn_plus = Button(buttons_frame, text='+')
+btn_plus.grid(row=3, column=3, padx=(1), pady=1)
+
+mainloop()
